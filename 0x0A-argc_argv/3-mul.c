aea83afb3d627a95db21a9e6adf59dac37f82880
ChegdalZack <zakariachegdal@gmail.com>
2023-04-10 10:29:41 -0700

    0x0A-argc_argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,7 +7,7 @@
  *
  * Return: the int converted from the string
  */
-int _atoi(char*s)
+int _atoi(char *s)
 {
 	int i, d, n, len, f, digit;
 
@@ -18,7 +18,7 @@ int _atoi(char*s)
 	f = 0;
 	digit = 0;
 
-	while (s[len] !='\0')
+	while (s[len] != '\0')
 		len++;
 	while (i < len && f == 0)
 	{
